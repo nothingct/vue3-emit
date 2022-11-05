6d8661a13dade67c0fe151e792854a340896c658
nothingct <nothingct@naver.com>
2022-11-06 02:09:11 +0900

    vue3-emit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MyBtn
+    @nothingct="log"
+    @change-msg="logMsg">
+    Banana
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/Mybtn';
+
+export default {
+  components: {
+    MyBtn
+  },
+  methods : {
+    log(event){
+      console.log('click!!')
+      console.log(event)
+    },
+    logMsg(msg){
+      console.log(msg)
+    }
+  }
+}
+</script>
+
---
+++ src/components/MyBtn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="btn">
+    <slot></slot>
+  </div>
+  <h1
+    @dblclick="$emit('nothingct',$event)">
+    HERE
+  </h1>
+  <input
+    type="text"
+    :v-model="msg" />
+</template>
+
+<script>
+export default {
+  emits:[
+    'nothingct',
+    'changeMsg'
+  ],
+  data(){
+    return{
+      msg: ''
+    }
+  },
+  watch:{
+    msg(){
+      this.$emit('changeMsg',this.msg);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .btn{
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color:white;
+    cursor : pointer;
+  }
+</style>
